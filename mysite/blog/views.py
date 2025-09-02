3195a875d9a36fdf91f80593fcee2eed69997bad
Mei-py <websocket.warrior@gmail.com>
2025-09-02 23:33:27 +0300

    добавлена система тегирования, посты извлеаются по схожести

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,13 +6,15 @@ from django.http import Http404
 from django.views.generic import ListView
 from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by= 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by= 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_share(request, post_id):
@@ -75,12 +77,17 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_post = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_post = similar_post.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {'post': post,
          'comments': comments,
-         'form': form}
+         'form': form,
+         'similar_posts': similar_post}
     )
 
 
@@ -111,22 +118,27 @@ def post_comment(request, post_id):
     )
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
     
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
-#     return render(
-#         request,
-#         'blog/post/list.html',
-#         {'posts': posts}
-#     )
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts,
+         'tag':tag}
+    )
 
